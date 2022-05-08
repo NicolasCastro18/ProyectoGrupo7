9d45049db1d19e8c736f53760facb7ddf2b97a13
Nicolas Castro Cervantes 82117 <93951746+NicolasCastro18@users.noreply.github.com>
2022-05-08 17:30:08 -0300

    subida

---
+++ ProyectoTeorico_TiendaSingapur/ProyectoTeorico_TiendaSingapur/Formularios De Negocios/Localidad/frmAltaLocalidad.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace ProyectoTeorico_TiendaSingapur.Formularios_De_Negocios.Localidad
+{
+    partial class frmAltaLocalidad
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblAltaLocalidad = new System.Windows.Forms.Label();
+            this.dgvAltaArticulo = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAltaArticulo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAgregar.Location = new System.Drawing.Point(171, 143);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.TabIndex = 9;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(126, 94);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(223, 20);
+            this.txtNombre.TabIndex = 8;
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.lblNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombre.Location = new System.Drawing.Point(66, 97);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(54, 13);
+            this.lblNombre.TabIndex = 7;
+            this.lblNombre.Text = "Nombre:";
+            // 
+            // lblAltaLocalidad
+            // 
+            this.lblAltaLocalidad.AutoSize = true;
+            this.lblAltaLocalidad.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.lblAltaLocalidad.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAltaLocalidad.Location = new System.Drawing.Point(66, 62);
+            this.lblAltaLocalidad.Name = "lblAltaLocalidad";
+            this.lblAltaLocalidad.Size = new System.Drawing.Size(286, 17);
+            this.lblAltaLocalidad.TabIndex = 6;
+            this.lblAltaLocalidad.Text = "Ingrese nombre de la nueva localidad:";
+            // 
+            // dgvAltaArticulo
+            // 
+            this.dgvAltaArticulo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvAltaArticulo.Location = new System.Drawing.Point(46, 36);
+            this.dgvAltaArticulo.Name = "dgvAltaArticulo";
+            this.dgvAltaArticulo.Size = new System.Drawing.Size(315, 146);
+            this.dgvAltaArticulo.TabIndex = 5;
+            // 
+            // frmAltaLocalidad
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(401, 223);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.lblAltaLocalidad);
+            this.Controls.Add(this.dgvAltaArticulo);
+            this.Name = "frmAltaLocalidad";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frmAltaLocalidad";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAltaArticulo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblAltaLocalidad;
+        private System.Windows.Forms.DataGridView dgvAltaArticulo;
+    }
+}
---
+++ ProyectoTeorico_TiendaSingapur/ProyectoTeorico_TiendaSingapur/Formularios De Negocios/TipoArticulo/frmAltaTipoArticulo.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace ProyectoTeorico_TiendaSingapur.Formularios_De_Negocios.TipoArticulo
+{
+    partial class frmAltaTipoArticulo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvAltaArticulo = new System.Windows.Forms.DataGridView();
+            this.lblAltaAticulo = new System.Windows.Forms.Label();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAltaArticulo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvAltaArticulo
+            // 
+            this.dgvAltaArticulo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvAltaArticulo.Location = new System.Drawing.Point(41, 35);
+            this.dgvAltaArticulo.Name = "dgvAltaArticulo";
+            this.dgvAltaArticulo.Size = new System.Drawing.Size(299, 146);
+            this.dgvAltaArticulo.TabIndex = 0;
+            // 
+            // lblAltaAticulo
+            // 
+            this.lblAltaAticulo.AutoSize = true;
+            this.lblAltaAticulo.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.lblAltaAticulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAltaAticulo.Location = new System.Drawing.Point(52, 51);
+            this.lblAltaAticulo.Name = "lblAltaAticulo";
+            this.lblAltaAticulo.Size = new System.Drawing.Size(261, 17);
+            this.lblAltaAticulo.TabIndex = 1;
+            this.lblAltaAticulo.Text = "Ingrese nombre del nuevo articulo:";
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.lblNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombre.Location = new System.Drawing.Point(55, 88);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(54, 13);
+            this.lblNombre.TabIndex = 2;
+            this.lblNombre.Text = "Nombre:";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(115, 85);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(187, 20);
+            this.txtNombre.TabIndex = 3;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAgregar.Location = new System.Drawing.Point(150, 131);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.TabIndex = 4;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // frmAltaTipoArticulo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(380, 214);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.lblAltaAticulo);
+            this.Controls.Add(this.dgvAltaArticulo);
+            this.Name = "frmAltaTipoArticulo";
+            this.Text = "frmAltaTipoArticulo";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAltaArticulo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvAltaArticulo;
+        private System.Windows.Forms.Label lblAltaAticulo;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Button btnAgregar;
+    }
+}
